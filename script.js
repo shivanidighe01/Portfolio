17b54be3cb42e9d29c164802694f5f04766dd342
shivanidighe01 <shivanidighe2003@gmail.com>
2025-04-16 16:35:30 +0530

    ui update

--- script.js
+++ script.js
@@ -24,4 +24,36 @@ typewriter.typeString('Engineering.')
     
 
       
-      
+  
+      const menuBtn = document.getElementById('menu-btn');
+      const navContent = document.querySelector('.nav-content');
+      const navLinks = document.querySelectorAll('.nav-content a');
+      const menuIcon = menuBtn.querySelector('i');
+    
+      menuBtn.addEventListener('click', () => {
+        navContent.classList.toggle('show');
+        menuIcon.classList.toggle('fa-bars');
+        menuIcon.classList.toggle('fa-times'); // Toggle icon
+      });
+    
+      navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+          navContent.classList.remove('show');
+          menuIcon.classList.add('fa-bars');
+          menuIcon.classList.remove('fa-times');
+        });
+      });
+    
+      // Smooth scroll
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          const target = document.querySelector(this.getAttribute('href'));
+          if (target) {
+            target.scrollIntoView({
+              behavior: 'smooth'
+            });
+          }
+        });
+      });
+        
